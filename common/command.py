be48c3982f69c6ee3c964c6f6d62b1df1d05de4f
Hammy <sgoudham@gmail.com>
2021-05-01 20:15:26 +0100

    Refactor classes out to files

---
+++ common/command.py
@@ -0,0 +1,6 @@
+class Command:
+    """Specify constants for client actions on given server"""
+
+    GET = "get"
+    PUT = "put"
+    LIST = "list"
---
+++ common/logger.py
@@ -0,0 +1,31 @@
+class Logger:
+    """
+    A class representing a Logger which logs information to the client and server
+    """
+
+    def __init__(self, host, port):
+        """
+        Constructor for Logger
+        :param host: Given host from User
+        :param port: Given port from User
+        """
+
+        self.connection = f"[{host}:{port}]"
+
+    def info(self, logger_string):
+        """
+        Print out to the console
+        :param logger_string: Specifies string to print out to console
+        :returns: None
+        """
+
+        print(f"{self.connection} -> {logger_string}")
+
+    def error(self, exception):
+        """
+        Print out unknown exception to the console
+        :param exception: Type of exception that occurred
+        :returns: None
+        """
+
+        print(f"{self.connection} -> Unknown Exception Occurred: {exception}")
