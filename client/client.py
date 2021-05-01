f8a69b5bc5cfa14a3d281f9425ddf7edf22d3811
Hammy <sgoudham@gmail.com>
2021-05-01 08:18:33 +0100

    Oop-ify client.py

--- client/client.py
+++ client/client.py
@@ -1,82 +1,62 @@
-import errno
-import socket
 import sys
 
-from util import Logger
+sys.path.append('..')
+from util import Logger, create_connection
 
 
-def get_client_arguments():
-    """
-    @:description Get information from client for connection setup, type of command and potentially a file
+class Client:
+    def __init__(self):
+        self.HOST = None
+        self.PORT = None
+        self.COMMAND = None
+        self.FILE = None
+        self.SOCKET = None
+        self.LOGGER = None
 
-    @:returns CLIENT_HOST (str): The host of the client
-    @:returns CLIENT_PORT (int): The port number of the client
-    @:returns CLIENT_COMMAND (str): The type of action the user inputs
-    @:returns CLIENT_FILE (str): The filename to download or upload (optional)
-    """
+        self.get_arguments()
 
-    CLIENT_HOST = None
-    CLIENT_PORT = None
-    CLIENT_COMMAND = None
-    CLIENT_FILE = None
+        self.LOGGER = Logger(self.HOST, self.PORT)
+        self.SOCKET = create_connection(self.HOST, self.PORT, "client", "server", self.LOGGER)
 
-    if len(sys.argv) >= 2:
-        CLIENT_HOST = sys.argv[1]
-    if len(sys.argv) >= 3:
-        CLIENT_PORT = sys.argv[2]
-    if len(sys.argv) >= 4:
-        CLIENT_COMMAND = sys.argv[3]
-    if len(sys.argv) >= 5:
-        CLIENT_FILE = sys.argv[4]
+    def get_arguments(self):
+        """
+        Get information from user for connection setup, type of command and potentially a file
+        """
 
-    return CLIENT_HOST, CLIENT_PORT, CLIENT_COMMAND, CLIENT_FILE
+        if len(sys.argv) >= 2:
+            self.HOST = sys.argv[1]
+        if len(sys.argv) >= 3:
+            self.PORT = sys.argv[2]
+        if len(sys.argv) >= 4:
+            self.COMMAND = sys.argv[3]
+        if len(sys.argv) >= 5:
+            self.FILE = sys.argv[4]
 
+    def handle(self):
+        """"""
 
-def create_connection(CLIENT_HOST, CLIENT_PORT, LOGGER):
-    """"""
-    client_sock = None
+        received = True
+        while received:
+            input_message = input("Please enter the message that you want to send: ")
 
-    try:
-        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_sock.connect((CLIENT_HOST, int(CLIENT_PORT)))
-    except socket.error as soe:
-        if soe.errno == errno.ECONNREFUSED:
-            LOGGER.info(soe)
-        sys.exit(1)
-    finally:
-        return client_sock
-
-
-def file_transfer(client_sock):
-    """"""
-
-    received = True
-    while received:
-        input_message = input("Please enter the message that you want to send: ")
-
-        if input_message == "EXIT":
-            received = False
-            client_sock.sendall(input_message.encode('utf-8'))
-        else:
-            client_sock.sendall(input_message.encode('utf-8'))
-            request = client_sock.recv(1024)
-
-            if request.decode('utf-8') == "EXIT":
+            if input_message == "EXIT":
                 received = False
+                self.SOCKET.sendall(input_message.encode('utf-8'))
             else:
-                print(request.decode('utf-8'))
+                self.SOCKET.sendall(input_message.encode('utf-8'))
+                request = self.SOCKET.recv(1024)
 
-    client_sock.close()
+                if request.decode('utf-8') == "EXIT":
+                    received = False
+                else:
+                    print(request.decode('utf-8'))
+
+        self.SOCKET.close()
 
 
 def main():
-    """Main Method For Client"""
-
-    CLIENT_HOST, CLIENT_PORT, CLIENT_COMMAND, CLIENT_FILE = get_client_arguments()
-    LOGGER = Logger(CLIENT_HOST, CLIENT_PORT)
-
-    client_sock = create_connection(CLIENT_HOST, CLIENT_PORT, LOGGER)
-    file_transfer(client_sock)
+    client = Client()
+    client.handle()
 
 
 if __name__ == "__main__":
