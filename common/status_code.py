d57054ed6830c8e51cba99b988ad996cdf2e2488
Hammy <sgoudham@gmail.com>
2021-05-03 14:34:26 +0100

    Add StatusCode class

--- common/status_code.py
+++ common/status_code.py
@@ -1,11 +1,14 @@
-class Error:
-    ONE = "[Errno-1] Arguments are Missing! E.g [python client.py <HOST> <PORT> <REQUEST> | python client.py <HOST> <PORT> <REQUEST> <FILE>]"
-    TWO = "[Errno-2] Given Command Not Recognised! Exiting Client [...]"
-    THREE = "[Errno-3] Given File Not Found! Exiting Client [...]"
-    FOUR = "[Errno-4] Timed Out - No Client Connection Received - Exiting Server [...]"
-    FIVE = " [Errno-5] File Transfer Failure! Terminating Connection [...]"
-    SIX = ""
+class StatusCode:
+    code = {
+        1000: "[CustomErrno 1000] Arguments Are Missing! E.g [python client.py <host> <port> <put file_name | get file_name | list>]",
+        1001: "[CustomErrno 1001] Given Command Not Recognised! Terminating Client [...]",
+        1002: "[CustomErrno 1002] Given File Not Found! Terminating Client [...]",
 
+        2000: "[CustomErrno 2000] Timed Out - Terminating Server [...]",
+        2001: "Listening For Client Connections [...]",
+        2002: "[CustomErrno 2002] Arguments Are Missing! E.g [python server.py <port>]",
 
-class Success:
-    ONE = "[Succ-1] File Transferred Successfully!"
+        3000: "[CustomErrno 3000] File Transfer Failure: ",
+
+        4000: "[CustomSucc 4000] File Transferred Successfully!"
+    }
