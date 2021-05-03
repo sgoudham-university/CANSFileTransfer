fa274b770fc4efdbc3a1f92e94cd02ba5d73e42c
Hammy <sgoudham@gmail.com>
2021-05-03 14:35:08 +0100

    Add error handling

--- server/server.py
+++ server/server.py
@@ -3,10 +3,10 @@ import socket
 import sys
 
 sys.path.append('..')
-from common.status_code import Error
+from common.status_code import StatusCode
 from common.command import Command
 from common.logger import Logger
-from common.util import create_connection, recv_header, write_file
+from common.util import create_connection, read_file, recv_message
 
 
 class Server:
@@ -26,11 +26,14 @@ class Server:
     def get_arguments(self):
         """
 
-        :return:
+        :returns:
         """
 
-        if len(sys.argv) >= 2:
+        try:
             self.PORT = sys.argv[1]
+        except IndexError:
+            print(StatusCode.code[2002])
+            sys.exit(1)
 
     def handle(self):
         """"""
@@ -38,39 +41,46 @@ class Server:
         HEADER_SIZE = 32
         SEPARATOR = ' ' * HEADER_SIZE
         STATUS_CODE = None
+        STATUS_MESSAGE = ""
 
         while True:
             try:
                 cli_socket, cli_address = self.SOCKET.accept()
             except socket.error:
-                self.LOGGER.status_code(Error.FOUR)
+                self.LOGGER.status_code(StatusCode.code[2000])
                 sys.exit(1)
             except Exception as exp:
                 self.LOGGER.unknown_error(exp)
                 sys.exit(1)
             else:
                 connection = True
-
                 while connection:
-
-                    header = recv_header(cli_socket, HEADER_SIZE, self.LOGGER)
+                    header, overflow = recv_message(cli_socket, HEADER_SIZE, self.LOGGER)
                     if not header: break
 
-                    REQUEST_TYPE, FILE_NAME, FILE_DATA_BYTES = header.split(SEPARATOR)
+                    request_type, file_name = header.split(SEPARATOR)
 
-                    if REQUEST_TYPE == Command.PUT:
-                        write_file(self, FILE_NAME, FILE_DATA_BYTES)
-                    elif REQUEST_TYPE == Command.GET:
+                    if request_type == Command.PUT:
+                        status = read_file(self, cli_socket, file_name, overflow, HEADER_SIZE)
+                        if not status:
+                            STATUS_CODE = 3000
+                            STATUS_MESSAGE = status
+                        else:
+                            STATUS_CODE = 4000
+
+                    elif request_type == Command.GET:
                         pass
                         # call send_file
-                    elif REQUEST_TYPE == Command.LIST:
+                    elif request_type == Command.LIST:
                         pass
 
+                    # message = f"{STATUS_CODE}{SEPARATOR}{STATUS_MESSAGE}"
+                    # header = f"{len(message):<{HEADER_SIZE}}" + message
+                    # cli_socket.sendall(header.encode('utf-8'))
+
                     connection = False
-
+                    self.LOGGER.status_code(StatusCode.code[2001])
                 cli_socket.close()
-
-            # call send_listing
 
 
 def main():
