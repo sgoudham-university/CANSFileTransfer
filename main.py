1a41a4d0a8ebef9accb609812c162a7e8daa2388
Hammy <sgoudham@gmail.com>
2021-05-05 20:44:55 +0100

    Add more file tests

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import os
 
 filecmp.clear_cache()
 print(filecmp.cmp(os.path.join("client", "data", "1GB.txt"), os.path.join("server", "data", "1GB.txt")))
+print(filecmp.cmp(os.path.join("client", "data", "empty_file.txt"), os.path.join("server", "data", "empty_file.txt")))
+print(filecmp.cmp(os.path.join("client", "data", "lorem_ipsum.txt"), os.path.join("server", "data", "lorem_ipsum.txt")))
+print(filecmp.cmp(os.path.join("client", "data", "1GB.txt"), os.path.join("server", "data", "1GB.txt")))
+print(filecmp.cmp(os.path.join("client", "data", "100MB.bin"), os.path.join("server", "data", "100MB.bin")))
