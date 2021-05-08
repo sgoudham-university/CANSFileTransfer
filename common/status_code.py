cc08dacc8eae5fe76b961f4178391d7573a508bc
Hammy <sgoudham@gmail.com>
2021-05-08 21:17:47 +0100

    Add new status codes and refactor old ones

--- common/status_code.py
+++ common/status_code.py
@@ -3,26 +3,34 @@ class StatusCode:
     This class contains a dictionary mapping of all the status codes within the program, they are classed as follows:
 
     1000 -> Status Codes for the Client
+
     2000 -> Status Codes for the Server
+
     3000 -> Error Codes shared between the Client & Server
+
     4000 -> Success Status Codes shared between the Client & Server
     """
 
     code = {
-        1000: "[CustomErrno 1000]: Arguments Are Missing! E.g [python client.py <host> <port> <put file_name | get file_name | list>]",
-        1001: "[CustomErrno 1001]: Given Command Not Recognised! Terminating Client [...]",
-        1002: "[CustomErrno 1002]: Given File Already Exists On Specified Server! Terminating Client [...]",
+        1000: "[ERROR 1000]: Arguments Are Missing! E.g [python client.py <host> <port> <put file_name | get file_name | list>]",
+        1001: "[ERROR 1001]: Given Command Not Recognised! Terminating Client [...]",
 
-        2000: "[CustomErrno 2000]: Timed Out - Terminating Server [...]",
+        2000: "[ERROR 2000]: Timed Out - Terminating Server [...]",
         2001: "Listening For Client Connections [...]",
-        2002: "[CustomErrno 2002]: Arguments Are Missing! E.g [python server.py <port>]",
+        2002: "[ERROR 2002]: Arguments Are Missing! E.g [python server.py <port>]",
 
-        3000: "Malformed File Header: ",
-        3001: "[CustomErrno 3001]: File Transfer Failure: ",
-        3003: "[CustomErrno 3003]: Could Not Send Status To Client!",
-        3004: "[CustomErrno 3004]: Given File Does Not Exist On Server! Terminating Connection [...]",
-        3005: "[CustomErrno 3005]: Given File Already Exists On Server! Terminating Connection [...]",
+        3000: "Malformed Request Message: ",
+        3001: "[ERROR 3001]: File Transfer Failure: ",
+        3003: "[ERROR 3003]: Could Not Send Status To Client!",
+        3004: "[ERROR 3004]: Given File Does Not Exist On Server! Terminating Connection [...]",
+        3005: "[ERROR 3005]: Given File Already Exists On Server! Terminating Connection [...]",
+        3006: "[ERROR 3006]: Given File Does Not Exist On Client Machine! Terminating Connection [...]",
+        3007: "[ERROR 3007]: Given File Already Exists On Client Machine! Terminating Connection [...]",
 
-        4000: "[CustomSucc 4000]: File Header Received Properly: ",
-        4001: "[CustomSucc 4001]: Request Successful: "
+        4000: "[SUCCESS 4000]: Request Type Received Properly: ",
+        4001: "[SUCCESS 4001]: File Information Sent Successfully: ",
+        4002: "[SUCCESS 4002]: Request Successful: ",
+        4003: "[SUCCESS 4003]: Request + File Are Valid: ",
+        4004: "[SUCCESS 4004]: Client Ready To Receive File",
+        4005: "[SUCCESS 4005]: Client File Exists: "
     }
