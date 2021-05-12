29cc4398fca4ff016e7a7cfd3e925770278f6be4
Hammy <sgoudham@gmail.com>
2021-05-12 01:58:25 +0100

    Add new status codes

--- common/status_code.py
+++ common/status_code.py
@@ -34,5 +34,7 @@ class StatusCode:
         4003: "[SUCCESS 4003]: Ready For File Transfer: ",
         4004: "[SUCCESS 4004]: Client Ready To Receive File",
         4005: "[SUCCESS 4005]: Client File Exists: ",
-        4006: "[SUCCESS 4006]: Server Directory Size Received Successfully: ",
+        4006: "[SUCCESS 4006]: Client Received Server Directory Size Successfully: ",
+        4007: "[SUCCESS 4007]: Client Ready To Receive Server Directory Size: ",
+        4008: "[SUCCESS 4008]: Server Ready To Receive File Information: "
     }
